3dc21ae2f9154e71270be4d3c76e1ec44a29daab
Shubh Karman Singh Bhullar <77555752+bhullarshubh@users.noreply.github.com>
2022-07-06 20:15:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function ShowDiv1()
+{
+  	document.getElementById("dropdown1").style.display="block";
+}
+function HideDiv1()
+{
+  	document.getElementById("dropdown1").style.display="none";
+}
+function ShowDiv2()
+{
+  	document.getElementById("dropdown2").style.display="block";
+}
+function HideDiv2()
+{
+  	document.getElementById("dropdown2").style.display="none";
+}
+function ShowDiv3()
+{
+  	document.getElementById("dropdown3").style.display="block";
+}
+function HideDiv3()
+{
+  	document.getElementById("dropdown3").style.display="none";
+}
+function ShowDiv4()
+{
+  	document.getElementById("dropdown4").style.display="block";
+}
+function HideDiv4()
+{
+  	document.getElementById("dropdown4").style.display="none";
+}
+function ShowDiv5()
+{
+  	document.getElementById("dropdown5").style.display="block";
+}
+function HideDiv5()
+{
+  	document.getElementById("dropdown5").style.display="none";
+}
